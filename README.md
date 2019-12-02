0f83601fa007ae56a9a756331f14227f326989e5
jenn jiang <yanjunjiang@cca.edu>
2019-12-01 19:57:16 -0800

    pushing project

--- README.md
+++
@@ -1,3 +0,0 @@
-# jennjiang.github.io
-Project x is my internship project on Moving brands, 2019
-Coded With Muhammad Usmanity 
--- js/mouse.js
+++
@@ -1,7 +0,0 @@
-if($(window).width() > 800) {
-  $(document).mousemove(function(event){
-  $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
-
-});
-
-}
